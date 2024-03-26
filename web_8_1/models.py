82d2f9ead3bbc2202c6845aab024b6680c34082a
ValeraPrus <146940361+ValeraPrus@users.noreply.github.com>
2024-03-26 22:42:08 +0200

    Add files via upload

---
+++ web_8_1/models.py
@@ -0,0 +1,27 @@
+from bson import json_util
+from mongoengine import connect, Document, StringField, ReferenceField, ListField, CASCADE
+
+connect(
+    db="WebHW",
+    host="mongodb+srv://walker:sobaka94@cluster0.tvae42i.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+)
+
+
+class Author(Document):
+    fullname = StringField(required=True, unique=True)
+    born_date = StringField(max_length=50)
+    born_location = StringField(max_length=150)
+    description = StringField()
+    meta = {"collection": "authors"}
+
+
+class Quote(Document):
+    author = ReferenceField(Author, reverse_delete_rule=CASCADE)
+    tags = ListField(StringField(max_length=15))
+    quote = StringField()
+    meta = {"collection": "quotes"}
+
+    def to_json(self, *args, **kwargs):
+        data = self.to_mongo(*args, **kwargs)
+        data["author"] = self.author.fullname
+        return json_util.dumps(data, ensure_ascii=False)
